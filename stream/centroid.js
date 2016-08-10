2e3a47154973c22773c6dd7e215d0a7dcfa1bd2e
missinglink <insomnia@rcpt.at>
2016-08-10 17:43:30 +0200

    add km distance calc to geojson props

--- stream/centroid.js
+++ stream/centroid.js
@@ -5,18 +5,26 @@ var through = require('through2'),
 
 // https://github.com/turf-junkyard/turf-along
 // https://github.com/turf-junkyard/turf-line-distance
-var UNIT = 'degrees';
+var UNIT = 'km';
 
 function centroid(){
   return through.obj( function( geojson, _, next ){
     try {
+
+      // total distance in km
       var dist = distance( geojson, UNIT );
+      geojson.properties.distance = dist;
+
+      // interpolate middle of path
       var point = along( geojson, dist/2, UNIT );
       geojson.properties.centroid = point.geometry.coordinates;
+
       this.push( geojson );
+
     } catch( e ){
       console.error( 'polyline centroid error', e );
     }
+
     next();
   });
 }
